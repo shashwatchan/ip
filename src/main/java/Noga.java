c40e3d87d7175237757991443dada6177a58add5
Shashwat Chandra <shashwat@Shashwats-MacBook-Air.local>
2025-01-24 13:08:37 +0800

    add echo

--- src/main/java/Noga.java
+++ src/main/java/Noga.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Noga {
     public static void main(String[] args) {
         System.out.println("Hello I am Noga, what can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")){
+                break;
+            }
+            System.out.println(userInput);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
